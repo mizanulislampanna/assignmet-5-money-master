f567d2503f76bf041b4d318e6b931d6e5c3ecc16
Mizanul Islam Panna <mizanulislampanna>
2022-02-17 11:51:34 +0600

    first event add

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+// common functions
+function getInputValue(inputName){
+    const inputText = document.getElementById(inputName);
+    const inputValue = inputText.value;
+   const inputNumber = parseInt(inputValue);
+   inputText.value = '';
+   return inputNumber;
+}
+function setInnerText(element, text){
+    const balanceText = document.getElementById(element);
+    balanceText.innerText = text;
+}
+
 document.getElementById('calculate-btn').addEventListener('click', function(){
-    console.log('click');
+   const income = getInputValue('income-input');
+   const food = getInputValue('food-input');
+   const rent = getInputValue('rent-input');
+   const cloths = getInputValue('cloths-input');
+    // expenses calculation 
+    const totalExpences = food + rent + cloths;
+    const balance = income - totalExpences;
+
+    console.log(totalExpences, balance);
+    // total expenses output
+    setInnerText('total-expenses',totalExpences);
+    //balance output
+    setInnerText('balance',balance);
 })
