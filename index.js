404a65891057790cb600643739572614cc07ba95
Mizanul Islam Panna <mizanulislampanna>
2022-02-17 12:56:20 +0600

    second event done

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-// common functions
+// common function for Income input 
+function getIncomeValue(inputName){
+    const inputText = document.getElementById(inputName);
+    const inputValue = inputText.value;
+   const inputNumber = parseInt(inputValue);
+   return inputNumber;
+};
+// common functions for input values
 function getInputValue(inputName){
     const inputText = document.getElementById(inputName);
     const inputValue = inputText.value;
    const inputNumber = parseInt(inputValue);
    inputText.value = '';
    return inputNumber;
-}
+};
+// common functions for outputs
 function setInnerText(element, text){
     const balanceText = document.getElementById(element);
     balanceText.innerText = text;
-}
-
+};
+// event add on calculate button 
 document.getElementById('calculate-btn').addEventListener('click', function(){
-   const income = getInputValue('income-input');
+   const income = getIncomeValue('income-input');
    const food = getInputValue('food-input');
    const rent = getInputValue('rent-input');
    const cloths = getInputValue('cloths-input');
@@ -20,9 +28,19 @@ document.getElementById('calculate-btn').addEventListener('click', function(){
     const totalExpences = food + rent + cloths;
     const balance = income - totalExpences;
 
-    console.log(totalExpences, balance);
     // total expenses output
     setInnerText('total-expenses',totalExpences);
     //balance output
     setInnerText('balance',balance);
+});
+
+// event add on save button
+document.getElementById('save-btn').addEventListener('click',function(){
+    const income = getIncomeValue('income-input');
+    const save = getInputValue('save-input');
+    const saveAmount = (income/100) * save ;
+    const remainningBalence = income - saveAmount;
+    // saving output
+    setInnerText('saving-amount',saveAmount);
+    setInnerText('remainning-balance',remainningBalence);
 })
